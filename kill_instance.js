42a33c62e23d1e8cf6af2d283535ae0237df7097
Drithyl <hugo.bujan@gmail.com>
2019-02-25 11:20:57 -0500

    Fixed a conditional bug.

--- kill_instance.js
+++ kill_instance.js
@@ -11,51 +11,52 @@ module.exports.kill = function(spawnedInstance, cb)
     maxTries = 3;
   }
 
-  if (spawnedInstance != null || spawnedInstance.killed !== true)
+  if (spawnedInstance == null || spawnedInstance.killed === true)
   {
-    (function killAttempt()
-    {
-      //The signal SIGKILL seems to set the flag .killed to true
-      //in instances even before they are terminated, so it's a valid check
-      if (spawnedInstance == null || spawnedInstance.killed === true)
-      {
-        //success
-        cb(null);
-        return;
-      }
-
-      if (tries > maxTries)
-      {
-        //if an exitCode exists and it is not 0, it is likely that this instance was bugged,
-        //and was not working properly in the first place
-        if (spawnedInstance.exitCode != null && spawnedInstance.exitCode !== 0)
-        {
-          rw.logError({exitCode: spawnedInstance.exitCode, instance: spawnedInstance}, `The game instance is still not killed after ${maxTries} attempts. It seems that the instance contained an error.`);
-          cb(`The game instance is still not killed after ${maxTries} attempts. It seems that the instance contained an error.`, null);
-        }
-
-        else
-        {
-          rw.logError({instance: spawnedInstance}, `The game instance is still not killed after ${maxTries} attempts.`);
-          cb(`The game instance is still not killed after ${maxTries} attempts.`, null);
-        }
-
-        return;
-      }
-
-      tries++;
-
-      //stdin must be paused for a node instance to exit
-      if (spawnedInstance.stdin != null)
-      {
-        spawnedInstance.stdin.pause();
-      }
-
-      //The SIGKILL signal is the one that kills a process
-      spawnedInstance.kill("SIGKILL");
-      setTimeout(killAttempt, 600);
-    })();
+    cb(null);
+    return;
   }
 
-  else cb(null);
+  (function killAttempt()
+  {
+    //The signal SIGKILL seems to set the flag .killed to true
+    //in instances even before they are terminated, so it's a valid check
+    if (spawnedInstance == null || spawnedInstance.killed === true)
+    {
+      //success
+      cb(null);
+      return;
+    }
+
+    if (tries > maxTries)
+    {
+      //if an exitCode exists and it is not 0, it is likely that this instance was bugged,
+      //and was not working properly in the first place
+      if (spawnedInstance.exitCode != null && spawnedInstance.exitCode !== 0)
+      {
+        rw.logError({exitCode: spawnedInstance.exitCode, instance: spawnedInstance}, `The game instance is still not killed after ${maxTries} attempts. It seems that the instance contained an error.`);
+        cb(`The game instance is still not killed after ${maxTries} attempts. It seems that the instance contained an error.`, null);
+      }
+
+      else
+      {
+        rw.logError({instance: spawnedInstance}, `The game instance is still not killed after ${maxTries} attempts.`);
+        cb(`The game instance is still not killed after ${maxTries} attempts.`, null);
+      }
+
+      return;
+    }
+
+    tries++;
+
+    //stdin must be paused for a node instance to exit
+    if (spawnedInstance.stdin != null)
+    {
+      spawnedInstance.stdin.pause();
+    }
+
+    //The SIGKILL signal is the one that kills a process
+    spawnedInstance.kill("SIGKILL");
+    setTimeout(killAttempt, 600);
+  })();
 }
