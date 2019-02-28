111dc85bc20f7623e3e51cc7e139c7f007b0ba32
Drithyl <hugo.bujan@gmail.com>
2019-02-28 13:35:07 -0500

    Added a backup script that gets called through the --preexec and --postexec command line arguments of Dominions.

---
+++ backup_script.js
@@ -0,0 +1,77 @@
+require("./prototype_functions.js");
+const fs = require("fs");
+const config = require("./config.json");
+const rw = require("./reader_writer.js");
+const preexecRegex = new RegExp("^\\-\\-preexec$", "i");
+const postexecRegex = new RegExp("^\\-\\-postexec$", "i");
+
+var gameName = process.argv[2];
+var type = process.argv[3];
+var source = `${config.dom5DataPath}/savedgames/${gameName}`;
+var target = `${config.pathToGameSaveBackup}/`;
+var turn;
+
+rw.log(["backup"], `Backup type ${type} for ${gameName} starting.`);
+
+if (gameName == null)
+{
+  rw.log(["error", "backup"], `No game name argument received.`);
+}
+
+if (preexecRegex.test(type) === true)
+{
+  target += `${config.latestTurnBackupDirName}/${gameName}/Turn ${turn}`;
+}
+
+else if (postexecRegex.test(type) === true)
+{
+  target += `${config.newTurnsBackupDirName}/${gameName}/Turn ${turn}`;
+}
+
+else
+{
+  rw.log(["error", "backup"], `Backup type received is invalid; expected --preexec or --postexec: ${type}`);
+  return;
+}
+
+fs.readFile(`${config.statusPageBasePath}/${gameName}_status`, "utf8", (err, content) =>
+{
+  if (err)
+  {
+    rw.log(["error", "backup"], `Error occurred while reading file ${config.statusPageBasePath}/${gameName}_status:`, err);
+    return;
+  }
+
+  content = content.match(/turn \d+/i);
+
+  if (content[0] == null)
+  {
+    rw.log(["error", "backup"], `Content matched is incorrect: ${content}`);
+    return;
+  }
+
+  turn = content[0].replace(/\D+/gi, "");
+
+  if (isNaN(+turn) === true)
+  {
+    rw.log(["error", "backup"], `Turn parsed is incorrect: ${turn}`);
+    return;
+  }
+
+  //statuspages don't update fast enough to give the new turn number right after
+  //the turn processes, therefore add 1 to it
+  if (postexecRegex.test(type) === true)
+  {
+    turn++;
+  }
+
+  rw.copyDir(source, target, false, ["", ".2h", ".trn"], function(err)
+  {
+    if (err)
+    {
+      rw.log(["error", "backup"], `Error occurred while copying dir ${source} to ${target}.`);
+    }
+
+    rw.log("backup", `${gameName}'s ${type} Turn ${turn} backed up successfully.`);
+  });
+});
